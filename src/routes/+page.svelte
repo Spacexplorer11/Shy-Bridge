a3361de6fbcedb481489a51e59a4c4b0953f87a1
Akaalroop Singh <akaal@akaalroop.com>
2025-09-13 17:10:35 +0100

    Ran prettier
    Signed-off-by: Akaalroop Singh <akaal@akaalroop.com>

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,5 +2,5 @@
 </script>
 
 <div class="@container text-center">
-<h1 class="text-4xl md:text-5xl"><span class="hover:opacity-5">Shy Bridge</span></h1>
+	<h1 class="text-4xl md:text-5xl"><span class="hover:opacity-5">Shy Bridge</span></h1>
 </div>
